65cce93222b27c07226edf29381246055587305c
Alex <MyAlexEducation@gmail.com>
2019-02-10 12:30:39 +0300

    module3-task2

--- functions.php
+++ functions.php
@@ -17,7 +17,8 @@ function include_template($name, $data)
     return $result;
 };
 
-function countCategory ($elements, $category) {
+function countCategory($elements, $category)
+{
     $count = 0;
     foreach ($elements as $key => $item) {
         if ($item["category"] === $category) {
@@ -27,9 +28,18 @@ function countCategory ($elements, $category) {
     return $count;
 };
 
-function esc($str) {
+function esc($str)
+{
     $text = htmlspecialchars($str);
     //$text = strip_tags($str);
 
     return $text;
+};
+
+function isTaskTime($taskData)
+{
+    $currentTime = time();
+    $taskTime = strtotime($taskData);
+    $criticalTaskTime = 3600;  //количество секунд в часе
+    return $taskTime - $currentTime < $criticalTaskTime && $taskTime != 0;
 };
