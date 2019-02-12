067b1dc23296fd67565d1dd17ba9d6460fb726d9
Alex <MyAlexEducation@gmail.com>
2019-02-12 14:19:07 +0300

    module4-task1

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+USE doingsdone;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_of_registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email CHAR(128) NOT NULL UNIQUE,
+  name CHAR(128) NOT NULL UNIQUE,
+  password CHAR(128) NOT NULL
+);
+
+CREATE TABLE projects (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  title CHAR(128) NOT NULL
+);
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  project_id INT,
+  date_of_creation TIMESTAMP,
+  date_of_completion TIMESTAMP,
+  state TINYINT,
+  title CHAR(128),
+  critical_time TIMESTAMP
+);
