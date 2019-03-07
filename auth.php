63357e3bb1f51691c942b6f1c9b545fb7f3b015b
Alex <MyAlexEducation@gmail.com>
2019-03-07 17:30:20 +0300

    удаление лишнего

--- auth.php
+++ auth.php
@@ -41,11 +41,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($errors)) {
         $current_user_id = intval($user['id']);
         $_SESSION['user'] = $user;
-        $_SESSION['filters'] = [
-            'filter_categories' => 0,
-            'filter_task' => 'all',
-            'filter_completed' => false
-        ];
         header('Location: /');
     }
 }
